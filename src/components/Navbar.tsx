9fba22c149f4156315ed7ac917a6fc9fa3f4956b
jakejunior-tech <jakejunior2008@gmail.com>
2026-09-05 17:01:30 +0100

    Close mobile dropdown on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,10 @@ export function Navbar() {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    const onScroll = () => setScrolled(window.scrollY > 40);
+    const onScroll = () => {
+      setScrolled(window.scrollY > 40);
+      setOpen(false);
+    };
     onScroll();
     window.addEventListener("scroll", onScroll, { passive: true });
     return () => window.removeEventListener("scroll", onScroll);
